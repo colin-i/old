a99d9cfefee2a0dbe01e38311b630d9819930066
bc <costin.botescu@gmail.com>
2024-01-13 13:14:46 +0200

    wsb-gui build

--- wsb-gui/include/common.h
+++ wsb-gui/include/common.h
@@ -8,16 +8,16 @@ const TRUE=1
 const MAX_PATH=260
 
 const DWORD=4
-const QWORD=8
+#const QWORD=8
 
 
 const void=0
 
 
 Const SEEK_SET=0
-Const SEEK_CUR=1
+#Const SEEK_CUR=1
 Const SEEK_END=2
 
 
 const ENOENT=2
-#const EEXIST=17
+const EEXIST=17
